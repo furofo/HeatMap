aff77be9817161b3b279506796cb3a1f2e2d2756
furofo <chriscellphonerepair@gmail.com>
2020-03-23 23:41:44 -0400

    put title element in place

--- config.js
+++ config.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+function makeHeatMap(json) {
+console.log("this worked");
+console.log(json);
+};
+
+
+
+
+
+$(document).ready(function() {  
+    fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json')
+      .then(response => response.json())
+      .then(data => {
+        let json = JSON.parse(JSON.stringify(data));
+        makeHeatMap(json);
+      });  
+});
