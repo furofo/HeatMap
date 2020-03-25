f3ec983bd6339d442d87df16a09efe15098aa985
furofo <chriscellphonerepair@gmail.com>
2020-03-24 22:29:43 -0400

    made bars stack up vertically with even height

--- config.js
+++ config.js
@@ -2,13 +2,65 @@ function makeHeatMap(json) {
 let box = document.querySelector('svg');
 let w = box.clientWidth;
 let h = box.clientHeight;
-console.log("this is width " + w);
-console.log("this is height" + h);
 console.log(json);
+let months = [1,2,3,4,5,6,7,8,9,10,11,12];
+let finMonths = months.map(x => monthsConveter(x));
+var xScale = d3.scaleBand() // this is an ordinal scal band makes width of bars equal
+.domain(months) // takes values 1 - 9 maps one to x coordinate 0, 9 to width which is 600
+.range([0, w]);
+var yScale = d3.scaleBand()
+.domain(months)
+.range([0, h]);
+const svg = d3.select("svg");
+const g = svg.append('g');
+
+g.selectAll('rect')
+  .data(months)
+  .enter()
+  .append("rect")
+  .attr("x", (d,i) => {
+    console.log("this is xscalebad" + xScale(d));
+    return "0";
+  })
+  .attr("y", (d, i) => {
+    console.log("this is Y Scale Band " + yScale(d))
+    return yScale(d);
+  })
+  .attr("width", xScale.bandwidth())
+  .attr("height", yScale.bandwidth())
+  .attr("fill", "green");
 };
 
+function monthsConveter(month) {
+  
+  switch(month) {
+    case 1:
+    return 'January';
+    case 2:
+    return 'Febuary';
+    case 3:
+    return 'March';
+    case 4:
+    return 'April';
+    case 5:
+    return 'May';
+    case 6:
+    return 'June';
+    case 7:
+    return 'July';
+    case 8:
+    return 'August';
+    case 9:
+    return 'September';
+    case 10:
+    return 'October';
+    case 11:
+    return 'November';
+    case 12:
+    return 'December';
+  }
 
-
+  }
 
 
 $(document).ready(function() {  
@@ -19,3 +71,5 @@ $(document).ready(function() {
         makeHeatMap(json);
       });  
 });
+
+//adds (variance) to temperatue tool tip shows value rounded down to tenths of each
