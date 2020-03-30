fd04bd6f72a590c656b638b8fcbc4f891108f984
furofo <chriscellphonerepair@gmail.com>
2020-03-29 21:59:59 -0400

    condensed code passed more tests

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ function monthsConverter(month) {  // convert months from integer and return str
     return 'Please input valid number from 1 - 12';
   }
   }
-function jsonCombinedArr(json) { // returns an arr with sub array of formate [year, month] so i can use this to calculate domain and range for width and x and y placemnt
+function jsonCombinedArr(json) { // returns an obj with years key associated with arr of years and months with string verison of months so 1 is january etc
   let years = [];
   let months = [];
   let obj = {};
@@ -39,14 +39,13 @@ function jsonCombinedArr(json) { // returns an arr with sub array of formate [ye
   }
   obj.years = years;
   obj.months = months;
-  console.log("this is obj .years" + obj.years);
   return obj;
 }
 function makeHeatMap(json) {  //this provides logic for making graph
 let box = document.querySelector('svg');
 let w = box.clientWidth;
 let h = box.clientHeight;
-let margin = {top: 100, left: 70, right: 70, bottom: 30}; //adopt margin convention.
+let margin = {top: 100, left: 70, right: 70, bottom: 30}; //adopt margin convention give elements a little margin
 let combinedArr = jsonCombinedArr(json);
 var xScale = d3.scaleBand() // this is an ordinal scal band makes width of bars equal
 .domain(combinedArr.years) // takes values 1 - 9 maps one to x coordinate 0, 9 to width which is 600
@@ -71,13 +70,17 @@ g.selectAll('rect')
   })
   .attr("width", xScale.bandwidth())
   .attr("height", yScale.bandwidth())
-  .attr("fill", (d, i) => colorScale(combinedArr.months[i]));
+  .attr("fill", (d, i) => colorScale(combinedArr.months[i]))
+  .attr("class", "cell")
+  .attr("data-month", (d, i) =>  d.month - 1)
+  .attr("data-year", (d, i) => {
+    console.log("this is d.year" + d.year);
+    return d.year})
+  .attr("data-temp", (d, i) => d.variance);
 const xAxis = d3.axisBottom(xScale)
                 .tickValues(xScale.domain().filter(function(d,i){  
   // only show every 12 tick or every 3rdyear since there are 4 x values for every year and was crowding x-axis
-                    console.log("these are tick values" + d);
                     if(d % 10 == 0) {
-                      console.log("this is succes??? " + d);
                       return d;
                     }
                                                            
@@ -91,11 +94,11 @@ svg.append("g")
     .attr("transform", "translate(" + (margin.left) + ", 0)") // make y -axis
     .attr("id", "y-axis")
     .call(yAxis);
-//let legend = svg.append("g").attr("id", "legend")
-    //.attr("height", 100)
-   // .attr("width", width / 7)  // thi holds key to interpet colors for scatterplot
-   // .attr('transform', 'translate(' + (width - 100) + ',200)')
-    //.attr("id", "legend");
+let legend = svg.append("g").attr("id", "legend")
+    .attr("height", 100)
+    .attr("width", width / 7)  // thi holds key to interpet colors for scatterplot
+    .attr('transform', 'translate(' + (width - 100) + ',200)')
+    .attr("id", "legend");
 };
 $(document).ready(function() {  
     fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json')
